185cbd5d236e2732b70c682e49087107da77e071
Delyan Raychev <delyan@raychev.org>
2019-08-24 23:53:07 -0700

    update trainer scripts

--- scripts/trainer-init.sh
+++ scripts/trainer-init.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
+set -auexo pipefail
+
 sudo DEBIAN_FRONTEND=noninteractive apt-get -yq install git make emacs25-nox
-mkdir -p /home/$AZURE_USER/src
-cd src
 ssh-keyscan -H github.com >> ~/.ssh/known_hosts
+
+## Install NVidia drivers
+CUDA_REPO_PKG=cuda-repo-ubuntu1604_10.0.130-1_amd64.deb
+wget -O /tmp/${CUDA_REPO_PKG} http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/${CUDA_REPO_PKG}
+sudo dpkg -i /tmp/${CUDA_REPO_PKG}
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+rm -f /tmp/${CUDA_REPO_PKG}
+sudo apt-get -yq update
+sudo apt-get -yq install cuda-drivers cuda
+
+
+mkdir -p ~/src
+cd ~/src
 git clone git@github.com:delqn/generative-deep-learning.git || true
-cd /home/$AZURE_USER/src/generative-deep-learning
+cd ~/src/generative-deep-learning
 pip3 install virtualenv
 virtualenv generative
 source generative/bin/activate
