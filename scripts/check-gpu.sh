ce258926c632d97695a133bc0deae4a168bb9a76
Delyan Raychev <delyan@raychev.org>
2019-08-24 23:54:55 -0700

    Adding check GPU

---
+++ scripts/check-gpu.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+print('Tensorflow:')
+from tensorflow.python.client import device_lib
+print(device_lib.list_local_devices())
+
+print('Keras:')
+from keras import backend as K
+K.tensorflow_backend._get_available_gpus()
