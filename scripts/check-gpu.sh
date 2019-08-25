88565edd2b3f28cb9083ce50210b57ea606813d8
Delyan Raychev <delyan@raychev.org>
2019-08-25 00:05:19 -0700

    Remove dependency on tensorflow

--- scripts/check-gpu.sh
+++ scripts/check-gpu.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-print('Tensorflow:')
-from tensorflow.python.client import device_lib
-print(device_lib.list_local_devices())
+# print('Tensorflow:')
+# from tensorflow.python.client import device_lib
+# print(device_lib.list_local_devices())
 
 print('Keras:')
 from keras import backend as K
