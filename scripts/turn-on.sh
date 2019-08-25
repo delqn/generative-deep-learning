ef165e8305238524df07644cabd6faf3a9aeaf9e
Delyan Raychev <delyan@raychev.org>
2019-08-25 08:53:16 -0700

    Adding more scripts

--- scripts/turn-on.sh
+++ scripts/turn-on.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-set -aueo pipefail
+set -auexo pipefail
 
 # az login
 
 source .env
 
-#    --no-wait
-echo az vm stop \
+az vm start \
    --subscription $AZURE_SUBSCR \
    --resource-group $AZURE_RESOURCEGRP \
    --name $AZURE_VMNAME
 
-#    --no-wait
-echo "Deallocating VM..."
-echo az vm deallocate \
-   --subscription $AZURE_SUBSCR \
+IP=$(az vm show -d \
    --resource-group $AZURE_RESOURCEGRP \
-   --name $AZURE_VMNAME
+   --name $AZURE_VMNAME \
+   --query publicIps -o tsv)
+
+echo "VM $IP is up"
