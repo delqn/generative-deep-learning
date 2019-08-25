22fe9ddc34570bf6a03433b3af9068f59f69624b
Delyan Raychev <delyan@raychev.org>
2019-08-25 17:29:04 +0000

    Script to install libcudnn

--- scripts/install-libcudnn.sh
+++ scripts/install-libcudnn.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
+set -auexo pipefail
+
+CUDADIR=$(mktemp -d)
+CUDATGZ="cudnn-10.0-linux-x64-v7.4.2.24.tgz"
 # CUDNN
-wget https://developer.download.nvidia.com/compute/redist/cudnn/v7.4.2/cudnn-10.0-linux-x64-v7.4.2.24.tgz
-tar -xvf cudnn-10.0-linux-x64-v7.4.2.24.tgz
-cp include/cudnn.h /usr/local/cuda-10.0/include/cudnn.h
-cp lib64/libcudnn.so.7.4.2 /usr/local/cuda-10.0/lib64/libcudnn.so.7.4.2
+wget "https://developer.download.nvidia.com/compute/redist/cudnn/v7.4.2/$CUDATGZ"
+
+tar -xvf "$CUDATGZ" -C "$CUDADIR"
+
+sudo cp "$CUDADIR/cuda/include/cudnn.h" /usr/local/cuda-10.0/include/cudnn.h
+sudo cp "$CUDADIR/cuda/lib64/libcudnn.so.7.4.2" /usr/local/cuda-10.0/lib64/libcudnn.so.7.4.2
+
+sudo rm -rf /usr/local/cuda-10.0/lib64/libcudnn.so.7
+sudo rm -rf /usr/local/cuda-10.0/lib64/libcudnn.so
+
 sudo ln -s /usr/local/cuda-10.0/lib64/libcudnn.so.7.4.2 /usr/local/cuda-10.0/lib64/libcudnn.so.7
 sudo ln -s /usr/local/cuda-10.0/lib64/libcudnn.so.7 /usr/local/cuda-10.0/lib64/libcudnn.so
+
+rm -rf $CUDATGZ
+rm -rf $CUDADIR
